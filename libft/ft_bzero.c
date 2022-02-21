d9f1260cb858a5741486cb7ea5f3701cd0bb8981
Pedro Callegaro <83246404+PCallegaro@users.noreply.github.com>
2022-02-21 22:27:14 +0000

    Add files via upload

---
+++ libft/ft_bzero.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peantoni <peantoni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/18 21:00:17 by peantoni          #+#    #+#             */
+/*   Updated: 2022/02/18 23:10:44 by peantoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*De acordo com o man essa funcao escreve n zero bytes para esta string
+se n eh zero, essa funcao ft_bzero nao faz nada. Peguei a funcao anterior
+para realizar esta, pois ele faz a funcao de preencher qualquer string do
+tamanho n com 0 s.*/
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
+  
