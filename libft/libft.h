6884184b2995f6d0ab43ef83705dda3636cf95bd
Pedro Callegaro <83246404+PCallegaro@users.noreply.github.com>
2022-02-27 22:34:22 +0000

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -9,7 +9,6 @@
 /*   Updated: 2022/02/21 22:08:22 by peantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #ifndef LIBFT_H
 # define LIBFT_H
 
@@ -19,17 +18,30 @@
 # include <stdio.h>
 
 int	ft_isalnum(int c);
-int	ft_ft_isalpha(void);
+int	ft_isalpha(int arg);
 int	ft_isascii(int c);
 int	ft_isdigit(int k);
 int	ft_isprint(int arg);
-int	ft_strlen(char *c);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_toupper(int c);
 int	ft_tolower(int c);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+int	ft_atoi(const char *str);
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+
+size_t	ft_strlen(const char *c);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
-char	*strchr(const char *s, int c);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
