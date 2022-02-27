b1d3420708b73f144290bcfbaf2334a427e7511e
Pedro Callegaro <83246404+PCallegaro@users.noreply.github.com>
2022-02-27 22:31:28 +0000

    Add files via upload

---
+++ libft/ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peantoni <peantoni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/25 16:06:11 by peantoni          #+#    #+#             */
+/*   Updated: 2022/02/25 17:46:30 by peantoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	sign;
+
+	res = 0;
+	sign = 1;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-')
+		sign *= -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		if (res > 2147483647)
+			return (-1);
+		if (res < -2147483647)
+			return (0);
+		res = res * 10 + *str - '0';
+		str++;
+	}
+	return (res * sign);
+}
