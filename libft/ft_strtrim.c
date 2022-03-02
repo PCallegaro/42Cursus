ef2b5bc43f610ccda1e61fbbf9c02df704927b91
Pedro Callegaro <83246404+PCallegaro@users.noreply.github.com>
2022-03-02 22:50:02 +0000

    Add files via upload

---
+++ libft/ft_strtrim.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peantoni <peantoni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/02 20:42:22 by peantoni          #+#    #+#             */
+/*   Updated: 2022/03/02 22:36:19 by peantoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		i;
+	int		len;
+	char	*str;
+
+	if (!s1)
+		return (NULL);
+	i = 0;
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i ++;
+	len = ft_strlen(s1) - 1 ;
+	while (s1[len] && ft_strchr(set, s1[len]) && len > i)
+		len --;
+	str = ft_substr(s1, i, (len - i) + 1);
+	return (str);
+}
